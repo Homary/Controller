de314f9f654a12c3a278752cc59dc6809d247486
Homary <935124541@qq.com>
2018-12-21 09:02:49 +0800

    更新README

--- webpack.proxy.js
+++ webpack.proxy.js
@@ -6,6 +6,6 @@ module.exports = [{
         '/icon/*',
         '/instruction/*'
     ],
-    target: 'http://192.168.30.35:9090',
+    target: 'http://192.168.31.78:9090',
     changeOrigin: true
 }]
